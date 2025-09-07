cbb0532c35daf654228fab151488310aeab01c69
rmasonh25 <rmasonh25@gmail.com>
2025-09-07 14:51:20 -0400

    chage chat controller -- add options temperature and tokens max output

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 #### To test API use in powershell (windows) 
 
 ```
-(Invoke-WebRequest -Uri "http://localhost:8080/api/chat" 
--Method POST `
-  -Headers @{ "Content-Type" = "application/json" } 
--Body '{"question": "Whats the best time to write code?"}').Content
+(Invoke-WebRequest -Uri "http://localhost:8080/api/chat" `
+  -Method POST `
+  -Headers @{ "Content-Type" = "application/json" } `
+  -Body '{"question": "Whats the best time to write code?"}').Content
+
 ```
 
--- src/main/java/com/bujisoft/hello_sprin_ai/ChatBotController.java
+++ src/main/java/com/bujisoft/hello_sprin_ai/ChatBotController.java
@@ -4,13 +4,19 @@
  * Copyright (c) 2025 Bujisoft
  * Author: Robert Hendricks
  * File: ChatBotController.java
- * Created on: 2025-09-07 02:25
- * Last modified: 2025-09-07 02:25
+ * Created on: 2025-09-07 14:44
+ * Last modified: 2025-09-07 14:44
  *
  */
 
 package com.bujisoft.hello_sprin_ai;
 
+import com.google.common.io.ByteProcessor;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatOptions;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,8 +33,19 @@ public class ChatBotController {
 
     @PostMapping("/api/chat")
     public ChatBotResponse askQuestion(@RequestBody ChatBotRequest chatBotRequest) {
-        String userText = chatBotRequest.text();                 // never null/blank
-        String answer   = vertexAiGeminiChatModel.call(userText);
-        return new ChatBotResponse(userText, answer);
+        String question = chatBotRequest.question();
+
+        //// Setup options
+        ChatOptions options = VertexAiGeminiChatOptions.builder()
+                .maxOutputTokens(100)
+                .temperature(0.5)
+                .build();
+        Prompt prompt = new Prompt(question, options);
+        ///  Invoke Gemini Client
+        ChatResponse chatResponse = vertexAiGeminiChatModel.call(prompt);
+        ///  extract answer
+        String answer   = chatResponse.getResult().getOutput().getText();
+        ///  return response
+        return new ChatBotResponse(chatBotRequest.question(), answer);
     }
 }
